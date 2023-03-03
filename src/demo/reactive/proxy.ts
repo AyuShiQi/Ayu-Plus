c5a69fa31e68d1384ff343d0434b96c87ddccce0
Desu <673464228@qq.com>
2023-03-03 22:23:22 +0800

    trigger and track

--- src/demo/reactive/proxy.ts
+++ src/demo/reactive/proxy.ts
@@ -1,18 +1,42 @@
 /**
  * 这里是数据劫持流程
  */
-import { activeFn } from './register'
+import { activeFn, deps } from './register'
+import type { ActiveFn } from './register'
 
+// 副作用函数储存追踪
+const track = (target: object, key: any): void => {
+    if(!activeFn) return
+    // 找obj
+    let dep = deps.get(target)
+    if(!dep) deps.set(target, (dep = new Map<string, Set<ActiveFn>>()))
+    let bucket = dep.get(key)
+    if(!bucket) dep.set(key, (bucket = new Set<ActiveFn>()))
+    bucket.add(activeFn)
+}
+// 触发副作用函数
+const trigger = (target: object, key: any): void => {
+    let dep = deps.get(target)
+    if(!dep) return
+    let bucket = dep.get(key)
+    if(!bucket) return
+    bucket.forEach(item => {
+        if(item) item()
+    })
+}
 // reactive的实现
 export const reactive = (data: object): object => {
     const proxyObj = new Proxy(data, {
         get(target: object, key: any): object {
-            if(!activeFn) return Reflect.get(target, key)
             // 找寻deps的过程
+            track(target, key)
             return Reflect.get(target, key)
         },
         set(target: object, key: any, value: any): boolean {
-            return Reflect.set(target, key, value)
+            const res = Reflect.set(target, key, value)
+            if(!res) return false
+            trigger(target, key)
+            return true
         }
     })
     return proxyObj
--- src/demo/reactive/register.ts
+++ src/demo/reactive/register.ts
@@ -1,15 +1,19 @@
 /**
  * 这里是函数的注册流程
  **/
-type ActiveFn = ((...props: unknown[])=>any) | undefined
+export type ActiveFn = ((...props: unknown[])=>any) | undefined
 /**
  * 当前活跃状态副作用函数
  */
 export let activeFn: ActiveFn = undefined
+/**
+ * deps表单
+ */
+export const deps = new WeakMap<object, Map<string, Set<ActiveFn>>>()
 
 export const effect = (fn: ActiveFn): void => {
   const effectFn = () => {
-    activeFn = fn
+    activeFn = effectFn
     if(fn) fn()
   }
   effectFn()
