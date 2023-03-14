a3b64b0ed71eb5a55707e0fa3b593360bb258aca
Desu <673464228@qq.com>
2023-03-14 21:42:20 +0800

    renderer

--- src/demo/renderer/renderer.ts
+++ src/demo/renderer/renderer.ts
@@ -1,5 +1,5 @@
 export function createRenderer(options: any) {
-    const { createElement, insert, setElementText } = options
+    const { createElement, insert, setElementText, patchProps } = options
 
     function render(vnode: any,container: any) {
         if(vnode) {
@@ -11,18 +11,28 @@ export function createRenderer(options: any) {
     }
     
     function patch(oldVnode: any, vnode: any, container: any) {
-        if(oldVnode) {
-            mountElement(oldVnode, vnode, container)
+        if(!oldVnode) {
+            mountElement(vnode, container)
         }
         else {
-            container.innerHTML
+            // diff算法入口
         }
     }
 
-    function mountElement(oldVnode: any, vnode: any, container: any) {
-        const el = createElement(vnode.tag)
+    function mountElement(vnode: any, container: any) {
+        const el = createElement(vnode.type)
         if(typeof vnode.child === 'string') {
             setElementText(el, vnode.child)
+        }
+        else if(Array.isArray(vnode)) {
+            vnode.forEach((child: any) => {
+                patch(null, child, el)
+            })
+        }
+        if(vnode.props) {
+            for(const key in vnode.props) {
+                patchProps(el, key, null, vnode.props[key])
+            }
         }
         insert(el, container)
     }
@@ -31,3 +41,24 @@ export function createRenderer(options: any) {
         render
     }
 }
+
+function shouldSetAsProps(el: any, key: any, value: any): boolean {
+    if(key === 'form' && el.tagName === 'INPUT') return false
+    return key in el
+}
+
+const render = createRenderer({
+    patchProps(el: any, key: any, prevValue: any, nextValue: any) {
+        // DOM properties存在这个属性
+        if(shouldSetAsProps(el, key, nextValue)) {
+            const type = typeof el[key]
+            if(type === 'boolean' && nextValue === '') {
+                el[key] = true
+            }
+            else {
+                el[key] = nextValue
+            }
+        }
+        else el.setAttribute(key, nextValue)
+    }
+})
