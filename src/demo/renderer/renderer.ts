70b1a8b797149fd22c88077f45a28cb5c6216760
Desu <673464228@qq.com>
2023-03-15 20:15:24 +0800

    umount and mount

--- src/demo/renderer/renderer.ts
+++ src/demo/renderer/renderer.ts
@@ -1,26 +1,41 @@
 export function createRenderer(options: any) {
-    const { createElement, insert, setElementText, patchProps } = options
+    const { createElement, insert, setElementText, patchProps, unmount } = options
 
     function render(vnode: any,container: any) {
         if(vnode) {
             patch(container._vnode, vnode, container)
         }
         else {
-            container.innerHTML = ''
+            if(container._vnode) {
+                unmount(container._vnode)
+            }
         }
+        container._vnode = vnode
     }
     
     function patch(oldVnode: any, vnode: any, container: any) {
-        if(!oldVnode) {
-            mountElement(vnode, container)
+        if(oldVnode.type !== vnode.type) {
+            unmount(oldVnode)
+            // 以便后续重新挂载操作
+            oldVnode = null
         }
-        else {
-            // diff算法入口
+        const { type } = vnode
+        // 说明是个普通的节点
+        if(typeof type === 'string') {
+            // 没有老虚拟node，直接挂载
+            if(!oldVnode) {
+                mountElement(vnode, container)
+            }
+            else {
+                // diff算法入口
+            }
+        } else {
+            // 说明是个组件
         }
     }
 
     function mountElement(vnode: any, container: any) {
-        const el = createElement(vnode.type)
+        const el = vnode.el = createElement(vnode.type)
         if(typeof vnode.child === 'string') {
             setElementText(el, vnode.child)
         }
@@ -50,7 +65,9 @@ function shouldSetAsProps(el: any, key: any, value: any): boolean {
 const render = createRenderer({
     patchProps(el: any, key: any, prevValue: any, nextValue: any) {
         // DOM properties存在这个属性
-        if(shouldSetAsProps(el, key, nextValue)) {
+        if(key==='class') {
+            el.className = nextValue || ''
+        } else if(shouldSetAsProps(el, key, nextValue)) {
             const type = typeof el[key]
             if(type === 'boolean' && nextValue === '') {
                 el[key] = true
@@ -60,5 +77,10 @@ const render = createRenderer({
             }
         }
         else el.setAttribute(key, nextValue)
-    }
+    },
+    unmount(vnode: any) {
+        const el = vnode.el
+        const parent = el.parent
+        if(parent) parent.removeChild(el)
+    },
 })
